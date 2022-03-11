97fe905a4499ae0e648d0e820f4c512a23fae0b4
Bob F <bobfdev@gmail.com>
2022-03-10 23:59:28 -0600

    updated Footer section

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -55,7 +55,47 @@ export const FooterLink = styled.p`
     font-size: 14px;
 
     &:hover {
-        color: #01bf71;
+        color: #010606;
         transition: 0.3s ease-out;
     }
 `
+
+export const SocialMediaContainer = styled.section`
+    max-width: 1000px;
+    width: 100%;
+`;
+
+export const SocialMediaWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1100px;
+    margin: 40px auto 0 auto;
+
+    @media screen and (max-width: 820px) {
+        flex-direction: column;
+    }
+`;
+
+export const WebsiteRights = styled.small`
+    color: #fff;
+    margin-bottom: 16px;
+`;
+
+export const SocialIcons = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 240px;
+`;
+
+export const SocialIconLink = styled.a`
+    color: #fff;
+    font-size: 24px;
+    transition: 0.5s;
+
+    &:hover {
+        color: #010606;
+        transform: translateY(-10px);
+    }
+`;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,8 +5,21 @@ import {
   FooterLinksWrapper,
   FooterLinkItems,
   FooterLinkTitle,
-  FooterLink
+  FooterLink,
+  SocialMediaContainer,
+  SocialMediaWrapper,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink
 } from './FooterElements';
+import { 
+  FaFacebook,
+  FaInstagram,
+  FaTiktok,
+  FaTwitter,
+  FaYoutube
+} from 'react-icons/fa';
+
 
 function Footer() {
   return (
@@ -28,6 +41,48 @@ function Footer() {
                 </FooterLinkItems>
               </FooterLinksWrapper>
           </FooterLinksContainer>
+          <SocialMediaContainer>
+            <SocialMediaWrapper>
+              <WebsiteRights>214 Fitness &copy; {new Date().getFullYear()} All rights reserved.</WebsiteRights>
+              <SocialIcons>
+                <SocialIconLink
+                  href='//www.facebook.com/' 
+                  target='_blank'
+                  aria-label='Facebook'
+                >
+                  <FaFacebook />
+                </SocialIconLink>
+                <SocialIconLink
+                  href='//www.instagram.com/' 
+                  target='_blank'
+                  aria-label='Instagram'
+                >
+                  <FaInstagram />
+                </SocialIconLink>
+                <SocialIconLink
+                  href='//www.twitter.com/' 
+                  target='_blank'
+                  aria-label='Twitter'
+                >
+                  <FaTwitter />
+                </SocialIconLink>
+                <SocialIconLink
+                  href='//www.tiktok.com/' 
+                  target='_blank'
+                  aria-label='Tiktok'
+                >
+                  <FaTiktok />
+                </SocialIconLink>
+                <SocialIconLink
+                  href='//www.youtube.com/' 
+                  target='_blank'
+                  aria-label='Youtube'
+                >
+                  <FaYoutube />
+                </SocialIconLink>
+              </SocialIcons>
+            </SocialMediaWrapper>
+          </SocialMediaContainer>
         </FooterWrapper>
     </FooterContainer>
   )
